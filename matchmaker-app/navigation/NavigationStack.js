00d3e56a19b4f17e0e1a0c13c8acf791342fee92
Matthew Nieva <matthewcn56@gmail.com>
2022-01-22 11:05:10 -0800

    woo friends

--- matchmaker-app/navigation/NavigationStack.js
+++ matchmaker-app/navigation/NavigationStack.js
@@ -7,6 +7,8 @@ import { AuthContext } from "./AuthProvider";
 import { ActivityIndicator } from "react-native";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
+import { getDoc, doc } from "firebase/firestore";
+import { db } from "../db/firebaseFunctions";
 
 export default function NavigationStack() {
   const { user, setUser } = useContext(AuthContext);
@@ -16,11 +18,34 @@ export default function NavigationStack() {
   //handle user state changes
   useEffect(() => {
     const subscriber = firebase.auth().onAuthStateChanged(function (user) {
-      setUser(user);
-      console.log("User changed!");
-      console.log(user);
-      if (initializing) setInitializing(false);
-      setLoading(false);
+      // setUser(user);
+      try {
+        if (!user) {
+          setUser(null);
+          if (initializing) setInitializing(false);
+          setLoading(false);
+          console.log("User is null");
+          return;
+        }
+        //at this point, user is defined
+        const userRef = doc(db, "users", user.uid);
+        // const userSnapshot = await getDoc(userRef);
+        //console.log(user);
+        getDoc(userRef).then((userSnapshot) => {
+          if (userSnapshot.exists()) {
+            console.log(userSnapshot.data());
+            setUser(userSnapshot.data());
+          } else {
+            setUser(user);
+          }
+          if (initializing) setInitializing(false);
+          setLoading(false);
+        });
+        // console.log("User changed!");
+        // console.log(user);
+      } catch (err) {
+        console.error(err);
+      }
     });
     return subscriber; // unsubscribe on unmount
   }, []);
--- matchmaker-app/screens/HomeScreen.js
+++ matchmaker-app/screens/HomeScreen.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../navigation/AuthProvider";
 import styles from "../styles.js";
-import { Text, View, Button, Image } from "react-native";
+import { Text, View, Button, Image, TextInput } from "react-native";
+import { addFriend } from "../db/firebaseFunctions";
 
 export default function HomeScreen() {
   const { user, logout } = useContext(AuthContext);
   const [userName, setUserName] = useState("");
   const [profilePic, setProfilePic] = useState(null);
+  const [friendId, setFriendID] = useState("");
 
   useEffect(() => {
     setUserName(user.displayName);
@@ -17,7 +19,15 @@ export default function HomeScreen() {
     <View style={styles.container}>
       <Text>Welcome {userName}</Text>
       <Image style={styles.profileImage} source={{ uri: profilePic }} />
-
+      <TextInput
+        placeholder="Friend ID"
+        onChangeText={(text) => setFriendID(text)}
+        value={friendId}
+      />
+      <Button
+        onPress={() => addFriend(user.uid, friendId)}
+        title="Add Friend!"
+      />
       <Button onPress={logout} title="Log Out" />
     </View>
   );
