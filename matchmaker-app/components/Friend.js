d1d7f3d3534e8292af51d56a7eac4b4a6b193baa
Matthew Nieva <matthewcn56@gmail.com>
2022-01-23 07:06:46 -0800

    const lmao

--- matchmaker-app/components/Friend.js
+++ matchmaker-app/components/Friend.js
@@ -1,52 +1,62 @@
-import React, { useContext } from 'react';
-import { Text, Image, View, StyleSheet } from 'react-native';
+import React, { useContext } from "react";
+import { Text, Image, View, StyleSheet } from "react-native";
 import { AuthContext } from "../navigation/AuthProvider";
-import { sendFriendRequest } from '../db/firebaseFunctions';
+import { sendFriendRequest } from "../db/firebaseFunctions";
 import { Entypo } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
-import global from "../styles"
+import global from "../styles";
 
 //props: friend object
 export default function Friend(props) {
-    const { user, setUser } = useContext(AuthContext);
-    return(
-        <>
-            <TouchableOpacity onPress={() => {
-                props.setModalVisible(false)}}>
-                <View style={styles.smallContainer}>
-                    <Image style={styles.img} source={require("../assets/temp.png")} />
-                    <View>
-                        <Text numberOfLines={1} style={[global.text3, {marginTop: 10}]}></Text>
-                        {console.log(props.friend.displayName)}
-                        <Text>{props.friend.displayName}</Text>
-                    </View>
-                    
-                </View>
-            </TouchableOpacity>
+  const { user, setUser } = useContext(AuthContext);
+  return (
+    <>
+      <TouchableOpacity
+        onPress={() => {
+          props.setModalVisible(false);
+        }}
+      >
+        <View style={styles.smallContainer}>
+          <Image
+            style={styles.img}
+            source={
+              props.friend.profilePic
+                ? { uri: props.friend.profilePic }
+                : require("../assets/temp.png")
+            }
+          />
+          <View>
+            <Text
+              numberOfLines={1}
+              style={[global.text3, { marginTop: 10 }]}
+            ></Text>
+            {console.log(props.friend.displayName)}
+            <Text>{props.friend.displayName}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </>
 
-        </>
-        
-        //need send request button, name, photo there (if possible)
-        
-    )
+    //need send request button, name, photo there (if possible)
+  );
 }
 
 const styles = StyleSheet.create({
-    smallContainer: {
-      flexDirection:'row',
-      flexWrap: 'nowrap',
-      paddingVertical: 15,
-      borderBottomColor: "#EAEAEA",
-      borderBottomWidth: 0.5,
-      borderStyle: "solid",
-      marginLeft: 15,
-      width: 300,
-    },
-    img: {
-      width: 40,
-      height: 40,
-      borderRadius: 50,
-      marginRight: 15,
-    },
-  });
+  smallContainer: {
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    paddingVertical: 15,
+    borderBottomColor: "#EAEAEA",
+    borderBottomWidth: 0.5,
+    borderStyle: "solid",
+    marginLeft: 15,
+    width: 300,
+  },
+  img: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    marginRight: 15,
+  },
+});
--- matchmaker-app/components/SendToModal.js
+++ matchmaker-app/components/SendToModal.js
@@ -7,87 +7,93 @@ import {
   Dimensions,
   Image,
   TextInput,
-  Alert, Modal, Pressable
+  Alert,
+  Modal,
+  Pressable,
 } from "react-native";
-import {
-  getAllFriends,
-  getUserProfile
-} from "../db/firebaseFunctions";
+import { getAllFriends, getUserProfile } from "../db/firebaseFunctions";
 import global from "../styles.js";
 import Friend from "./Friend.js";
 
 export default function FriendModal(props) {
-  const { user, logout, friendUids, incomingFriendRequestUids } = useContext(AuthContext);
+  const { user, logout, friendUids, incomingFriendRequestUids } =
+    useContext(AuthContext);
   const [searchValue, setSearchValue] = useState("");
   const [friends, setFriends] = useState([]);
-  
 
   //set possible friends when first mounted
-  useEffect(()=> {
-    const grabFriends = async() => {
+  useEffect(() => {
+    const grabFriends = async () => {
       const friendsIDs = await getAllFriends(user.uid, friendUids); //TODO: change this to be a function that returns an array of objects
       const friends = friendsIDs.map(async (uid) => await getUserProfile(uid));
-      friendObjects = await Promise.all(friends)
-      setFriends(friendObjects); 
+      const friendObjects = await Promise.all(friends);
+      setFriends(friendObjects);
     };
-    grabFriends();   
-  },[])
+    grabFriends();
+  }, []);
 
   const renderFriends = () => {
     let tempFriends = [...friends]; //copy possible friends
-    if (searchValue !== ""){
+    if (searchValue !== "") {
       tempFriends = tempFriends.filter(
-        (friend) => friend.displayName.slice(0, searchValue.length).toLowerCase() === searchValue.toLowerCase() 
+        (friend) =>
+          friend.displayName.slice(0, searchValue.length).toLowerCase() ===
+          searchValue.toLowerCase()
       );
     }
 
-    return tempFriends.map((friendObj, index) => 
-      <Friend 
-      setModalVisible={props.setModalVisible}
-      friend={friendObj} 
-      onAccept={() => {
-        let newFriends = [
-          ...friends.slice(0, index),
-          ...friends.slice(index+1),
-        ]
-        setFriends(newFriends);
-      }}
+    return tempFriends.map((friendObj, index) => (
+      <Friend
+        setModalVisible={props.setModalVisible}
+        friend={friendObj}
+        onAccept={() => {
+          let newFriends = [
+            ...friends.slice(0, index),
+            ...friends.slice(index + 1),
+          ];
+          setFriends(newFriends);
+        }}
       />
-    )
-  }
+    ));
+  };
 
   return (
     <Modal
-        visible={props.modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          props.setModalVisible(!props.modalVisible);
-        }}>
-        <View style={styles.centeredView}>
-            <Text style={global.text2}>Send to a Friend</Text>
-            <TextInput
-            style={[global.input, {width: 300, marginTop: 20, marginVertical: 20}]}
-            placeholder="Search Name"
-            // onChangeText={(text) => setFriendName(text)}
-            // value={friendName}
-            />
+      visible={props.modalVisible}
+      onRequestClose={() => {
+        Alert.alert("Modal has been closed.");
+        props.setModalVisible(!props.modalVisible);
+      }}
+    >
+      <View style={styles.centeredView}>
+        <Text style={global.text2}>Send to a Friend</Text>
+        <TextInput
+          style={[
+            global.input,
+            { width: 300, marginTop: 20, marginVertical: 20 },
+          ]}
+          placeholder="Search Name"
+          // onChangeText={(text) => setFriendName(text)}
+          // value={friendName}
+        />
 
-            {renderFriends()}
+        {renderFriends()}
 
-            <Pressable
-              style={global.button}
-              onPress={() => props.setModalVisible(!props.modalVisible)}>
-              <Text style={global.text}>Cancel</Text>
-            </Pressable>
-        </View>
-      </Modal>
-  )
+        <Pressable
+          style={global.button}
+          onPress={() => props.setModalVisible(!props.modalVisible)}
+        >
+          <Text style={global.text}>Cancel</Text>
+        </Pressable>
+      </View>
+    </Modal>
+  );
 }
 
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
-})
+});
