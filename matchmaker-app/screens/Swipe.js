b53948aba0920fdb71b95154070393419a1b61a2
edmondywen <edmondywen@gmail.com>
2022-01-23 07:41:13 -0800

    Type Error x.uid

--- matchmaker-app/screens/Swipe.js
+++ matchmaker-app/screens/Swipe.js
@@ -1,29 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import global from "../styles.js";
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 
 import Profile from "../components/Profile.js";
 import { AuthContext } from "../navigation/AuthProvider.js";
-import { denyMatch, requestMatch } from "../db/firebaseFunctions.js";
+import { denyMatch, getUserProfile, requestMatch } from "../db/firebaseFunctions.js";
 
 export default function Swipe() {
+  const [allProfiles, setAllProfiles] = useState([]);
   const { user, toSwipe } = useContext(AuthContext);
-  const allProfiles = toSwipe.map((id) => (
-    <Profile swipe={true} id={id} key={id} />
-  ));
+  console.log(toSwipe);
+  useEffect(() => {
+    const grabProfiles = async () => {
+      const profiles = toSwipe.map(async (uid) => await getUserProfile(uid));
+      const profilesData = await Promise.all(profiles);
+      setAllProfiles(profilesData);
+    }
+    if (toSwipe.length >0){
+      grabProfiles();
+    }
+  }, [toSwipe]);
   const displayedProfile = allProfiles[toSwipe.length - 1];
   const displayedUid = toSwipe[toSwipe.length - 1];
   return (
     <View style={[global.container, { paddingVertical: 50 }]}>
       {/* <Text>swipe on your ships</Text> */}
       {toSwipe.length != 0 ? (
-        displayedProfile
-      ) : (
-        <Text style={global.text}>No more profiles!</Text>
-      )}
-      {toSwipe.length != 0 && (
         <View style={[styles.row, { marginTop: 20 }]}>
+          <Profile
+            key={displayedProfile.uid}
+            profile={displayedProfile}
+            />
           <TouchableOpacity
             style={styles.reqButton}
             onPress={() => {
@@ -40,7 +48,7 @@ export default function Swipe() {
             <Entypo name="check" size={55} color="#2F2F2F" />
           </TouchableOpacity>
         </View>
-      )}
+      ) : "No more profiles"}
     </View>
   );
 }
